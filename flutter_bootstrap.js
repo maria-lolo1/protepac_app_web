2dbc03ad6fb624e0197029db14ba9daf22237db5
Maria-Lolo <marialolo012006>
2026-03-28 15:20:15 -0300

    novas telas e correcoes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1044081462"
+    serviceWorkerVersion: "2343324708"
   }
 });
